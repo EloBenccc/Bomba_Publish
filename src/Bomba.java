03de40f7e00bdd9d67f1e21d17ce9932a5f70bef
EloBenccc <71230307+EloBenccc@users.noreply.github.com>
2020-09-15 21:49:55 +0200

    Revert "Odliczanie Bomba"
    This reverts commit d1bca757bf32257b709b944246d55599d13eda95.

--- src/Bomba.java
+++ src/Bomba.java
@@ -1,11 +1,4 @@
 public class Bomba {
-    public static void main(String args[]) {
 
-        int x = 10;
-        while(x > 0){
-            x--;
-            System.out.println("Bomba wybuchnie za ..." + x);
 
-        }
-    }
 }
